14e268b69ba38b2dcc5fb0df9ce4f699fc72c66c
unknown <nakitarayamajhi27@gmail.com>
2024-06-24 10:47:25 +0545

    FP without image

--- swadeshi-products/src/pages/ForgotPassword.jsx
+++ swadeshi-products/src/pages/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { forgotPasswordApi } from "../apis/Api";
 import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const ForgetPassword = () => {
   const [forgotPasswordEmail, setForgotPasswordEmail] = useState("");
@@ -33,7 +34,62 @@ const ForgetPassword = () => {
 
   return (
     <>
-      <section class="row min-vh-100 align-items-center">
+      <div className="flex h-screen mb-[5rem] mt-[10rem] justify-center">
+        <div className="w-full lg:w-1/2 flex items-center justify-center">
+          <div className="max-w-md w-full p-6">
+            <h1 className="text-2xl font-semibold mb-6 text-black text-center">
+              Forgot Your Password?
+            </h1>
+            <p className="text-center">
+              Please enter the email address associated with your account and We
+              will email you a link to reset your password.
+            </p>
+
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  onChange={handleForgotPasswordEmail}
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                />
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+                >
+                  Send Request
+                </button>
+              </div>
+            </form>
+
+            <div className="mt-4 text-sm text-gray-600 text-center">
+              <p>
+                Return to &nbsp;
+                <Link to="/register" className="text-black hover:underline">
+                  Login
+                </Link>
+              </p>
+            </div>
+            <p className="text-[8px] mt-5 flex items-center justify-center">
+              By clicking continue, you agree to our &nbsp;{" "}
+              <b>Terms of Service</b>
+              &nbsp; and &nbsp;
+              <b>Privacy Policy</b>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <section class="row min-vh-100 align-items-center">
         <div class="container py-5 d-flex justify-content-center">
           <div class="col col-md-10 col-sm-12 col-lg-8">
             <div class="card d-flex align-items-center shadow-lg">
@@ -61,7 +117,7 @@ const ForgetPassword = () => {
                         account and We will email you a link to reset your
                         password.
                       </p>
-                      {/* Email */}
+                     
                       <div class="form-outline mb-4">
                         <label class="form-label">Email address</label>
                         <input
@@ -96,7 +152,7 @@ const ForgetPassword = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   );
 };
